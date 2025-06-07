a918097e233b61576cb93cb9b27e888e2e7643b2
apple288 <116697476+apple288@users.noreply.github.com>
2025-06-07 22:29:02 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -32,10 +32,10 @@ CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的�
 
 # ====== 全局可配置参数（可直接在此处修改） ======
 USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+UUID = "235958cc-ce4b-45e9-8e0d-6ef906f78dfb"                     # UUID，留空则自动生成
+PORT = 40607                   # Vmess端口，留空或0则自动生成
+DOMAIN = "sl.swk1.ip-ddns.com"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiY2Q2OTUyZDhiZDg5NDkwODQzYWM1YjAwYWE1N2IyYTUiLCJ0IjoiNzBhNTEyNWItNzVhYy00ZGYyLTkwNTYtZmYyOGY2Yzg4MzMyIiwicyI6Ik9HUXpZamhtWkdVdE0yTTBOaTAwTldNMkxUa3laR0V0TnpOaE9XRTBPV1pqTXpZMSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
